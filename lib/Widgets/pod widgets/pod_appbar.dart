4afc8ea438d676d50b2421b219885b061534c7d2
andreea <mariaandreealavric@gmail.com>
2024-10-08 23:41:32 +0200

    pod aggiornato

--- lib/Widgets/pod widgets/pod_appbar.dart
+++ lib/Widgets/pod widgets/pod_appbar.dart
@@ -25,10 +25,10 @@ class CustomPodAppBar extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Parte sinistra: CircleAvatar, nome utente e job title
-          const Column(
+          Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children:[
-               Text(
+            children: [
+              const Text(
                 'Welcome in the Pod',
                 style: TextStyle(
                   color: Colors.black,
@@ -37,19 +37,15 @@ class CustomPodAppBar extends StatelessWidget {
                   fontFamily: 'KeplerStd', // Assicurati che il font sia specificato in pubspec.yaml
                 ),
               ),
-      ]
-    ),
-              Row(
-                children: [
-                  Text(
-                    userProfile.displayName,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+              const SizedBox(height: 8),
+              Text(
+                userProfile.displayName,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
               Text(
                 userProfile.jobTitle,
                 style: const TextStyle(
@@ -58,25 +54,21 @@ class CustomPodAppBar extends StatelessWidget {
                   fontStyle: FontStyle.italic,
                 ),
               ),
+              const SizedBox(height: 8),
               CircleAvatar(
                 radius: 23,
                 backgroundColor: Colors.red,
-                child:
-                CircleAvatar(
+                child: CircleAvatar(
                   radius: 20,
                   backgroundImage: NetworkImage(userProfile.photoUrl),
                 ),
               ),
-              const SizedBox(height: 8),
-
-              ),
-  ],
+            ],
           ),
-          // Parte destra: "Welcome in the Pod"
+          // Parte destra: Espansione e allineamento
           const Expanded(
             child: Align(
-              alignment: Alignment.topLeft,
-
+              alignment: Alignment.topRight,
             ),
           ),
         ],
