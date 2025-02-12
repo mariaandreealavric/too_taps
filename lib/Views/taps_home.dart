b291db14aa10d7b241592e1e9ba94786df8866da
andreea <mariaandreealavric@gmail.com>
2025-02-12 19:21:10 +0100

    integrati tutti i file per aspettare l'arrivo di unreal engine. ovvero mi manca il file da luca unreal_engine.aar

--- lib/Views/taps_home.dart
+++ lib/Views/taps_home.dart
@@ -3,7 +3,6 @@ import 'package:logger/logger.dart';
 import 'package:get/get.dart';
 
 import '../Widgets/Navigazione/navigazione.dart';
-import '../Widgets/unreal_widget.dart';
 import '../controllers/Contatori/scroll_counter.dart';
 import '../controllers/Contatori/touch_counter.dart';
 import '../controllers/profile_controller.dart';
@@ -20,6 +19,7 @@ class TapsHomePage extends StatefulWidget {
 
 class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+ // bool _isWelcomeMessageShown = false;
   int _selectedIndex = 0; // Indice del segnaposto selezionato
   final Logger _logger = Logger(); // Instance of Logger
 
@@ -60,20 +60,28 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
   Widget build(BuildContext context) {
     _logger.i('TapsHomePage: Building with userID ${widget.userID}');
 
+    // Use GetX for ThemeController
     final themeController = Get.find<ThemeController>();
     final profileController = Get.find<ProfileController>(); // GetX ProfileController
     final scrollCounter = Get.find<ScrollCounter>(); // Usa GetX per il ScrollCounter
     final touchCounter = Get.find<TouchCounter>(); // Usa GetX per il TouchCounter
 
+    // Controlla lo stato di caricamento del profilo
     if (profileController.isLoading.value) {
       return const Center(child: CircularProgressIndicator());
     }
 
     final profile = profileController.profile.value;
 
+    // Se il profilo non è caricato, mostra un indicatore di caricamento
     if (profile == null) {
       return const Center(child: CircularProgressIndicator());
     }
+
+    /*if (!_isWelcomeMessageShown) {
+      _isWelcomeMessageShown = true;
+      _showWelcomeSnackbar(profile.displayName); // Accedi a displayName tramite profile
+    }*/
 
     return GestureDetector(
       onTap: () {
@@ -90,14 +98,14 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
         },
         child: Scaffold(
           backgroundColor: Colors.black,
-          extendBodyBehindAppBar: true,
+          extendBodyBehindAppBar: true, // Estende il corpo dietro l'AppBar per renderla trasparente
           appBar: AppBar(
             backgroundColor: Colors.transparent,
             shadowColor: Colors.transparent,
             elevation: 0,
-            centerTitle: true,
+            centerTitle: true, // Centro il titolo
             title: const Text(
-              'Taps',
+              'Taps', // Scritta in piccolo "Taps"
               style: TextStyle(
                 fontSize: 20,
                 color: Colors.white,
@@ -120,15 +128,20 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
                   onTap: () {
                     touchCounter.incrementTouches(); // Incrementa il contatore dei tap
                   },
-                  child: const UnrealWidget(), // Mostra Unreal Engine invece dell'immagine
+                  //child: const UnrealWidget(), // Mostra Unreal Engine invece dell'immagine
+                  child: Image.asset(
+                    'assets/statue.png', // Percorso dell'immagine
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               Column(
                 children: [
                   const SizedBox(height: 80), // Spazio per l'AppBar trasparente
+                  // Aggiunta dei segnaposto scrollabili
                   GestureDetector(
                     onTap: () {
-                      touchCounter.incrementTouches();
+                      touchCounter.incrementTouches(); // Incrementa il contatore dei tap
                     },
                     child: SizedBox(
                       height: 20,
@@ -139,21 +152,21 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
                           return GestureDetector(
                             onTap: () {
                               setState(() {
-                                _selectedIndex = index;
+                                _selectedIndex = index; // Aggiorna l'indice selezionato
                               });
-                              touchCounter.incrementTouches();
+                              touchCounter.incrementTouches(); // Incrementa il contatore dei tap
                             },
                             child: AnimatedContainer(
                               duration: const Duration(milliseconds: 300),
                               margin: const EdgeInsets.symmetric(horizontal: 10),
-                              width: 60,
-                              height: 60,
+                              width: 60, // Dimensione del segnaposto
+                              height: 60, // Dimensione del segnaposto
                               decoration: BoxDecoration(
                                 image: DecorationImage(
                                   image: AssetImage(
                                     index == _selectedIndex
-                                        ? _images[1]
-                                        : _images[0],
+                                        ? _images[1] // Immagine attiva
+                                        : _images[0], // Immagine inattiva
                                   ),
                                   fit: BoxFit.contain,
                                 ),
@@ -167,16 +180,16 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
                   ),
                   Expanded(
                     child: Align(
-                      alignment: Alignment.bottomRight,
+                      alignment: Alignment.bottomRight, // Fissa il contenuto in basso a destra
                       child: Padding(
-                        padding: const EdgeInsets.all(16.0),
+                        padding: const EdgeInsets.all(16.0), // Aggiunge un po' di padding se necessario
                         child: Obx(() {
                           return RichText(
-                            textAlign: TextAlign.right,
+                            textAlign: TextAlign.right, // Allinea il testo a destra
                             text: TextSpan(
                               children: [
                                 const TextSpan(
-                                  text: 'Taps',
+                                  text: 'Taps', // Testo prima di andare a capo
                                   style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 80,
@@ -186,7 +199,7 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
                                   ),
                                 ),
                                 TextSpan(
-                                  text: '\n${touchCounter.touches.value}',
+                                  text: '\n${touchCounter.touches.value}', // A capo con il numero dei tocchi aggiornato dinamicamente
                                   style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 80,
@@ -202,13 +215,13 @@ class TapsHomePageState extends State<TapsHomePage> with SingleTickerProviderSta
                       ),
                     ),
                   ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Navigation(profile: profile), // Barra di navigazione fissa in basso
+                  ),
                 ],
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Navigation(profile: profile),
               ),
             ],
           ),
