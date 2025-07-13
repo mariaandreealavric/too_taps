81b6cbb5cd1299542ee53cf21eaed04b5212f250
mariaandreealavric <168376790+mariaandreealavric@users.noreply.github.com>
2025-07-13 22:56:24 +0200

    Add native event channel and docs

--- android/app/src/main/kotlin/com/example/too_taps/MyAccessibilityService.kt
+++ android/app/src/main/kotlin/com/example/too_taps/MyAccessibilityService.kt
@@ -3,17 +3,30 @@ package com.example.too_taps
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 import android.util.Log
+import io.flutter.plugin.common.MethodChannel
 
 class MyAccessibilityService : AccessibilityService() {
+    private lateinit var channel: MethodChannel
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        channel = MethodChannel(
+            MainActivity.flutterEngineInstance.dartExecutor.binaryMessenger,
+            "com.yourapp/native_events"
+        )
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         if (event == null) return
 
         when (event.eventType) {
             AccessibilityEvent.TYPE_VIEW_CLICKED -> {
                 Log.d("AccessibilityService", "Tocco registrato su: ${event.className}")
+                channel.invokeMethod("onTouchEvent", null)
             }
             AccessibilityEvent.TYPE_VIEW_SCROLLED -> {
                 Log.d("AccessibilityService", "Scroll registrato su: ${event.className}")
+                channel.invokeMethod("onScrollEvent", null)
             }
         }
     }
